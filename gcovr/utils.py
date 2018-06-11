f617a3b45dac8cf2416ca237cc4ec01b6cf1bddb
Xuesong Hu <xuesong@allienterprises.com>
2018-06-10 17:00:11 -0700

    Revert "Revert "fix the wrong function name""
    This reverts commit 27c18ba1b24f451c1c5436d4aaee4f51906e5e78.

--- gcovr/utils.py
+++ gcovr/utils.py
@@ -138,7 +138,7 @@ def link_walker(path, exclude_dirs):
     for root, dirs, files in os.walk(os.path.abspath(path), followlinks=True):
         for exc in exclude_dirs:
             for d in dirs:
-                m = exc.search(d)
+                m = exc.match(d)
                 if m is not None:
                     dirs[:] = [d for d in dirs if d is not m.group()]
         print(dirs)
